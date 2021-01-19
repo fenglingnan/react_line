49c9af25f95bdae5f95160057303c575ef72397e
fenglingnan <185975315@qq.com>
2021-01-19 22:40:09 +0800

    test

--- src/api/api.js
+++ src/api/api.js
@@ -105,6 +105,7 @@ function refresh(){
 //all表示传递所有的response，默认传递返回的data
 //err_back表示是否需要错误的回调，默认不回调
 export function get(url, params = {},config={},all=false,err_back=false) {
+	console.log(process.env.REACT_APP_NET)
 	return new Promise((resolve, reject) => {
 		instance({
 			...{
